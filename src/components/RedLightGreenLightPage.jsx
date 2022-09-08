17c40bd785a719bfda675e454d818a18ba8c4009
Andrew Osmond <andrew.osmond24@gmail.com>
2022-09-08 17:11:58 -0400

    Added styling and light button to red light green light page (#13)
    * Renamed some pages & added blank vocab and red light green light page
    
    * Added styling and light that flips from red to green on click

--- src/components/RedLightGreenLightPage.jsx
+++ src/components/RedLightGreenLightPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 function RedLightGreenLightPage(props) {
+    let nextColor = "#008450";
+
+    function flipLight() {
+        let element = document.getElementById('light');
+        ReactDOM.findDOMNode(element).style.backgroundColor = nextColor;
+        nextColor = nextColor === "#008450" ? "#B81D13" : "#008450";
+    }
+
     return (
-        <h1>
-            This page is where the Red Light Green Light Minigame will be.
-        </h1>
+        <div className="RedLightGreenLightPage">
+            <div className="PageHeader">
+                <h3 className="PageHeaderText">
+                    Red Light Green Light Minigame
+                </h3>
+            </div>
+            <button id="light" onClick={flipLight}></button>
+        </div>
     );
 }
 
