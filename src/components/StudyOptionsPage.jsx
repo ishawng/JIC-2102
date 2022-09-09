73ba53fa65e89e3f404ba1bdcf30b5333e7f7deb
Rina Yoo <63939135+rinayooart@users.noreply.github.com>
2022-09-09 13:29:27 -0400

    Moved files, added homepage (#16)

--- src/components/StudyOptionsPage.jsx
+++ src/components/StudyOptionsPage.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+ 
 
 function UnitPage(props) {
     return (
-        <div className="study-options-page">
+        <div style={{alignItems:'center', display:'flex'}}className="study-options-page">
+            <Link to={'/units'}>
+                    <button color= "#fcb0b6" className="btn btn-primary">
+                        {'Return to Units List Page'}
+                    </button>
+                </Link>
             <Link to={'/vocab-page'}>
-                <button className="btn btn-primary">
-                    {'Vocab'}
-                </button>
-            </Link>
+                    <button color= "#fcb0b6" className="btn btn-primary">
+                        {'Vocab'}
+                    </button>
+                </Link>
+            
             <Link to={'/red-light-green-light-page'}>
                 <button className="btn btn-primary">
                     {'Red Light Green Light Minigame'}
+                </button>
+            </Link>
+            <Link to={'/tug-of-war-page'}>
+                <button className="btn btn-primary">
+                    {'Tug of War Minigame'}
                 </button>
             </Link>
         </div>
