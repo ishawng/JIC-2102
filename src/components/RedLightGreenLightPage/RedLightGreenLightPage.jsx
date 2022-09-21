2c2682964439f3ac5b135fa8a149e8d3750d03bf
Andrew Osmond <aosmond3@gatech.edu>
2022-09-21 15:30:00 -0400

    Changed function to class and  have working submit answer feature

--- src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
+++ src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './RedLightGreenLightPage.css';
 
-function RedLightGreenLightPage(props) {
-    let nextColor = "#008450";
-    const questionsList = [
-        ["What is 1 + 1?", "2"],
-        ["What is your favorite language?", "Korean"],
-        ["How are you doing?", "good"]
-    ]
-    let currQuestionIndex = 0
-    let answerInput = ""
-
-    function flipLight() {
-        let element = document.getElementById('light');
-        ReactDOM.findDOMNode(element).style.backgroundColor = nextColor;
-        nextColor = nextColor === "#008450" ? "#B81D13" : "#008450";
+class RedLightGreenLightPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nextColor: "#008450",
+            currQuestionIndex: 0,
+            answerInput: "",
+            questionsList: [
+                ["What is 1 + 1?", "2"],
+                ["What is your favorite language?", "Korean"],
+                ["How are you doing?", "good"]
+            ]
+        };
+        this.flipLight = this.flipLight.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    function startGame() {
+    flipLight() {
+        const element = document.getElementById('light');
+        ReactDOM.findDOMNode(element).style.backgroundColor = this.state.nextColor;
+        const nextColorUpdate = this.state.nextColor === "#008450" ? "#B81D13" : "#008450";
+        this.setState({nextColor: nextColorUpdate});
+    }
+
+    startGame() {
         /*
             credit to https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
             for shuffleArray function
@@ -29,43 +39,44 @@ function RedLightGreenLightPage(props) {
                 [array[i], array[j]] = [array[j], array[i]];
             }
         }
-        shuffleArray(questionsList)
-        console.log(questionsList)
+        shuffleArray(this.state.questionsList);
+        console.log(this.state.questionsList);
     }
 
-    function handleChange(event) {
-        answerInput = event.target.value
-        console.log(answerInput)
+    handleChange(event) {
+        this.setState({answerInput: event.target.value});
+        console.log(this.state.answerInput);
     }
 
-    function handleSubmit(event) {
-        alert("The following answer was submitted: " + answerInput)
-        event.preventDefault()
+    handleSubmit(event) {
+        alert("The following answer was submitted: " + this.state.answerInput);
+        event.preventDefault();
     }
 
-    return (
-        <div className="RedLightGreenLightPage">
-            <div className="PageHeader">
-                <h3 className="PageHeaderText">
-                    Red Light Green Light Minigame
-                </h3>
+    render() {
+        return (
+            <div className="RedLightGreenLightPage">
+                <div className="PageHeader">
+                    <h3 className="PageHeaderText">
+                        Red Light Green Light Minigame
+                    </h3>
+                </div>
+                <button id="light" onClick={this.flipLight}></button>
+                <button onClick={this.startGame}>Start Game</button>
+                <div className="Question">
+                    <h4>Question:</h4>
+                    <p>{this.state.questionsList[this.state.currQuestionIndex][0]}</p>
+                    <h4>Answer:</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <input type="text" value={this.state.answerInput} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
-            <button id="light" onClick={flipLight}></button>
-            <button onClick={startGame}>Start Game</button>
-            <div className="Question">
-                <h4>Question:</h4>
-                <p>{questionsList[currQuestionIndex][0]}</p>
-                <h4>Answer:</h4>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" value={answerInput} onChange={handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default RedLightGreenLightPage;
