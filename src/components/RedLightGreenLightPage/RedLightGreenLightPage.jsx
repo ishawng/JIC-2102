64d505cb18ed90fb0af6f3ed0efcc4bedd6e3c0f
Andrew Osmond <aosmond3@gatech.edu>
2022-09-27 16:54:27 -0400

    Switched RedLightGreenLightPage to functional component

--- src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
+++ src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
@@ -1,85 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { shuffleArray } from '../../utils';
 import './RedLightGreenLightPage.css';
 
-class RedLightGreenLightPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nextColor: "#008450",
-            currQuestionIndex: 0,
-            answerInput: "",
-            questionsList: [
-                ["What is 1 + 1?", "2"],
-                ["What is your favorite language?", "Korean"],
-                ["How are you doing?", "good"]
-            ]
-        };
-        this.flipLight = this.flipLight.bind(this);
-        this.startGame = this.startGame.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+function RedLightGreenLightPage() {
+    const [nextColor, setNextColor] = useState("#008450");
+    const [currQuestionIndex, setCurrQuestionIndex] = useState(0);
+    // const [answerInput, setAnswerInput] = useState('');
+    const [questionsList, setQuestionsList] = useState([
+        ["What is 1 + 1?", "2"],
+        ["What is your favorite language?", "Korean"],
+        ["How are you doing?", "good"]
+    ]);
+
+    function flipLight() {
+        ReactDOM.findDOMNode(document.getElementById("light")).style.backgroundColor = nextColor;
+        const nextColorUpdate = nextColor === "#008450" ? "#B81D13" : "#008450";
+        setNextColor(nextColorUpdate);
     }
 
-    flipLight() {
-        ReactDOM.findDOMNode(document.getElementById("light")).style.backgroundColor = this.state.nextColor;
-        const nextColorUpdate = this.state.nextColor === "#008450" ? "#B81D13" : "#008450";
-        this.setState({ nextColor: nextColorUpdate });
-    }
-
-    startGame() {
-        shuffleArray(this.state.questionsList);
+    function startGame() {
         console.log("startGame called")
-        console.log(this.state.questionsList);
+        shuffleArray(questionsList);
 
         ReactDOM.findDOMNode(document.getElementById("question-div")).style.visibility = "visible";
         ReactDOM.findDOMNode(document.getElementById("start-game-button")).style.visibility = "hidden";
     }
 
-    handleChange(event) {
-        this.setState({ answerInput: event.target.value });
-        console.log(this.state.answerInput);
-    }
+    function submitAnswer() {
+        const submission = document.getElementById('answer-input').value;
+        document.getElementById('answer-input').value = '';
+        console.log(submission);
 
-    handleSubmit(event) {
-        const submission = this.state.answerInput;
-        if (submission === this.state.questionsList[this.state.currQuestionIndex][1]) {
-            this.flipLight();
-            this.setState({ currQuestionIndex: this.state.currQuestionIndex + 1 })
+        if (submission === questionsList[currQuestionIndex]) {
+
         } else {
-            alert("Incorrect, try again")
-        }
-        // TODO: implement logic that occurs after correct and incorrect answer choice
 
-        event.preventDefault();
+        }
+
+
     }
 
-    render() {
-        return (
-            <div className="Background">
-                {/* <div className="PageHeader">
-                    <h3 className="PageHeaderText">
-                        Red Light Green Light Minigame
-                    </h3>
-                </div> */}
-                <button id="light" onClick={this.flipLight}></button>
-                <br></br>
-                <button id="start-game-button" onClick={this.startGame}>Start Game</button>
-                <div id="question-div">
-                    <h4>Question:</h4>
-                    <p>{this.state.questionsList[this.state.currQuestionIndex][0]}</p>
-                    <h4>Answer:</h4>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            <input type="text" value={this.state.answerInput} onChange={this.handleChange} />
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
+    return (
+        <div className="Background">
+            <button id="light"></button>
+            <br></br>
+            <button id="start-game-button" onClick={startGame}>Start Game</button>
+            <div id="question-div">
+                <h4>Question:</h4>
+                <p>{questionsList[currQuestionIndex][0]}</p>
+                <h4>Answer:</h4>
+                <div id='answer'>
+                    <input id='answer-input' type='text' autoComplete='off' />
+                    <button className='btn btn-primary' onClick={submitAnswer}>Submit</button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default RedLightGreenLightPage;
