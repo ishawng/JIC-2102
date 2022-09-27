a0c4a6be27e99c13ffd563e791f94c6f7d6059f4
Andrew Osmond <andrew.osmond24@gmail.com>
2022-09-27 19:10:53 -0400

    Red Light Green Light Improvements (#30)

--- src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
+++ src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
@@ -1,85 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import { shuffleArray } from '../../utils';
+import { getVocab } from '../../vocabData'
+import { ReactComponent as CircleSvg } from '../../SharedImages/Circle.svg'
+import { ReactComponent as SkullSvg } from '../../SharedImages/Skull.svg'
 import './RedLightGreenLightPage.css';
 
-class RedLightGreenLightPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nextColor: "#008450",
-            currQuestionIndex: 0,
-            answerInput: "",
-            questionsList: [
-                ["What is 1 + 1?", "2"],
-                ["What is your favorite language?", "Korean"],
-                ["How are you doing?", "good"]
-            ]
-        };
-        this.flipLight = this.flipLight.bind(this);
-        this.startGame = this.startGame.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+function ScoreView() {
+    return (
+        <>
+            <CircleSvg />
+            <CircleSvg />
+            <SkullSvg />
+        </>
+    );
+}
+
+
+function RedLightGreenLightPage() {
+    const [nextColor, setNextColor] = useState("#008450");
+    const [currQuestionIndex, setCurrQuestionIndex] = useState(0);
+    const [currCorrectScore, setCurrCorrectScore] = useState(-1); // set as -1 so update to 0 in startGame() triggers rerender
+    const [currIncorrectScore, setCurrIncorrectScore] = useState(-1);
+    const questions = getVocab(1);
+
+    function flipLight() {
+        document.getElementById("light").style.backgroundColor = nextColor;
+        const nextColorUpdate = nextColor === "#008450" ? "#B81D13" : "#008450";
+        setNextColor(nextColorUpdate);
     }
 
-    flipLight() {
-        ReactDOM.findDOMNode(document.getElementById("light")).style.backgroundColor = this.state.nextColor;
-        const nextColorUpdate = this.state.nextColor === "#008450" ? "#B81D13" : "#008450";
-        this.setState({ nextColor: nextColorUpdate });
-    }
-
-    startGame() {
-        shuffleArray(this.state.questionsList);
-        console.log("startGame called")
-        console.log(this.state.questionsList);
-
-        ReactDOM.findDOMNode(document.getElementById("question-div")).style.visibility = "visible";
-        ReactDOM.findDOMNode(document.getElementById("start-game-button")).style.visibility = "hidden";
-    }
-
-    handleChange(event) {
-        this.setState({ answerInput: event.target.value });
-        console.log(this.state.answerInput);
-    }
-
-    handleSubmit(event) {
-        const submission = this.state.answerInput;
-        if (submission === this.state.questionsList[this.state.currQuestionIndex][1]) {
-            this.flipLight();
-            this.setState({ currQuestionIndex: this.state.currQuestionIndex + 1 })
-        } else {
-            alert("Incorrect, try again")
+    function startGame() {
+        shuffleArray(questions);
+        setCurrCorrectScore(0);
+        setCurrIncorrectScore(0);
+        setCurrQuestionIndex(0);
+        for (let i = 0; i < 3; i++) {
+            document.getElementById("score-view").childNodes[i].classList.remove("red");
         }
-        // TODO: implement logic that occurs after correct and incorrect answer choice
 
-        event.preventDefault();
+        document.getElementById("start-game-button").style.visibility = "hidden";
+        document.getElementById("postgame-div").style.visibility = "hidden";
+        document.getElementById("question-div").style.visibility = "visible";
     }
 
-    render() {
-        return (
-            <div className="Background">
-                {/* <div className="PageHeader">
-                    <h3 className="PageHeaderText">
-                        Red Light Green Light Minigame
-                    </h3>
-                </div> */}
-                <button id="light" onClick={this.flipLight}></button>
-                <br></br>
-                <button id="start-game-button" onClick={this.startGame}>Start Game</button>
-                <div id="question-div">
-                    <h4>Question:</h4>
-                    <p>{this.state.questionsList[this.state.currQuestionIndex][0]}</p>
-                    <h4>Answer:</h4>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            <input type="text" value={this.state.answerInput} onChange={this.handleChange} />
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
+    function submitAnswer() {
+        const submission = document.getElementById('answer-input').value;
+        document.getElementById('answer-input').value = '';
+
+        if (submission === questions[currQuestionIndex].korean) {
+            // console.log("CORRECT");
+
+            setCurrCorrectScore(currCorrectScore + 1);
+        } else {
+            // console.log("Question: " + questions[currQuestionIndex].english + "; Answer: " + questions[currQuestionIndex].korean)
+            // console.log("your incorrect answer: " + submission);
+
+            document.getElementById('score-view').childNodes[currIncorrectScore].classList.add('red');
+            setCurrIncorrectScore(currIncorrectScore + 1);
+            if (currIncorrectScore === 2) {
+                document.getElementById("question-div").style.visibility = "hidden";
+                document.getElementById("postgame-div").style.visibility = "visible";
+                document.getElementById("lose-text").style.visibility = "visible";
+            }
+        }
+        setCurrQuestionIndex((currQuestionIndex + 1) % questions.length);
+
+    }
+
+    console.log(questions[currQuestionIndex]);
+
+    return (
+        <div className="Background">
+            {console.log("rerender")}
+            <button id="light"></button>
+            <br></br>
+            <button id="start-game-button" onClick={startGame}>Start Game</button>
+            <div id="question-div">
+                <div id="score-view">
+                    <ScoreView />
+                </div>
+                <h4>Question:</h4>
+                <h2>What is <span id="question-text">{questions[currQuestionIndex].english}</span> in Korean?</h2>
+                <h4>Answer:</h4>
+                <div id='answer'>
+                    <input id='answer-input' type='text' autoComplete='off' />
+                    <button className='btn btn-primary' onClick={submitAnswer}>Submit</button>
                 </div>
             </div>
-        );
-    }
+            <div id='postgame-div'>
+                <h2 id='win-text'>You win!</h2>
+                <h2 id='lose-text'>You lose!</h2>
+                <button className='btn btn-primary' onClick={startGame}>Play Again</button>
+            </div>
+        </div>
+    );
+
 }
 
 export default RedLightGreenLightPage;
