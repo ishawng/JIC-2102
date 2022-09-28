c965f9d023b9cba8ec58f7d5a917e3bfe58f47ea
Rina Yoo <rinyou321@gmail.com>
2022-09-28 17:44:01 -0400

    Added homepage art, updated homepage UI

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { UNITS_LIST_PATH } from '../../constants';
 import { Button } from './Button';
+import homepageArt from './homepageArt.png';
 import './Home.css';
 
 function Home() {
     return (
         <div className='home-container'>
-            {/* image */}
-            <h1>KOREAN LANGUAGE GAME</h1>
-            <div className="home-play">
-                <Link to={UNITS_LIST_PATH}>
-                    <Button className='btn' buttonStyle='btn-primary' buttonSize='btn-large'>
-                        PLAY
-                    </Button>
-                </Link>
+            <div className='left-home-section'>
+                <div className='title'><h1>KOREAN <br/> LANGUAGE <br/> GAME</h1></div>
+                <div className='caption'><h6>Whether you're here for class or just for learning the basic Korean language, there's a spot for you in our game</h6></div>
+                <div className="home-play">
+                    <Link to={UNITS_LIST_PATH}>
+                        <Button className='btn' buttonStyle='btn-primary' buttonSize='btn-large'>
+                            PLAY
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+            <div className="right-home-section">
 
             </div>
+            
         </div>
     )
 }
