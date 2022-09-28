68a29b82aa1698b3c1f0d9b28e3d10e0e87b1188
Kelly, James Hong In <jkelly90@gatech.edu>
2022-09-28 13:03:08 -0400

    Added Flashcards to Study Menu

--- src/components/FlashcardPage/FlashcardStudyPage.jsx
+++ src/components/FlashcardPage/FlashcardStudyPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { getVocab } from '../../vocabData';
 import FlashcardList from './FlashcardList';
 import './FlashcardStudyPage.css'
+
 
 function FlashcardStudyPage() {
   const [cards] = useState(SAMPLE_CARDS)
@@ -10,19 +12,6 @@ function FlashcardStudyPage() {
   );
 }
 
-const SAMPLE_CARDS = [
-  {
-    english: 'name',
-    korean: '이름',
-  },
-  {
-    english: 'truly, really, very',
-    korean: '참',
-  },
-  {
-    english: 'science',
-    korean: '사이언스',
-  }
-]
+const SAMPLE_CARDS = getVocab(1)
 
 export default FlashcardStudyPage;
