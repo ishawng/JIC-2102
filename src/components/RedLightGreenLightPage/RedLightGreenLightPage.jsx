c401cf057d66afe3d13780056eb14fd7c1dc983f
Andrew Osmond <andrew.osmond24@gmail.com>
2022-10-31 11:43:23 -0400

    Got rlgl pause on red light working (#73)

--- src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
+++ src/components/RedLightGreenLightPage/RedLightGreenLightPage.jsx
@@ -24,18 +24,19 @@ function RedLightGreenLightPage() {
     const EASY_QUESTION_RESPONSE_TIME = 12000;
     const MEDIUM_QUESTION_RESPONSE_TIME = 8000;
     const HARD_QUESTION_RESPONSE_TIME = 5000;
+    const RED_LIGHT_PAUSE_TIME = 2500;
     const location = useLocation();
     const unit = location.state;
     const [questionResponseTime, setQuestionResponseTime] = useState(5000);
     const [currQuestionIndex, setCurrQuestionIndex] = useState(0);
-    const [currCorrectScore, setCurrCorrectScore] = useState(-1); // set as -1 so update to 0 in startGame() triggers rerender
+    const [currCorrectScore, setCurrCorrectScore] = useState(-1); // set as -1 so update to 1 in startGame() triggers rerender
     const [currIncorrectScore, setCurrIncorrectScore] = useState(0);
     const questions = getVocab(1);
 
     function startGame(time) {
         setQuestionResponseTime(time);
         shuffleArray(questions);
-        setCurrCorrectScore(0);
+        setCurrCorrectScore(1);
         setCurrIncorrectScore(0);
         setCurrQuestionIndex(0);
         for (let i = 0; i < 3; i++) {
@@ -60,20 +61,24 @@ function RedLightGreenLightPage() {
                 if (prevScoreTotal === currCorrectScore + currIncorrectScore) {
                     document.getElementById("light").style.backgroundColor = RED;
                     document.getElementById('score-view').childNodes[currIncorrectScore].classList.add('red');
+                    document.getElementById("question-div").style.display = "none";
                     setCurrIncorrectScore(currIncorrectScore + 1);
                     if (currIncorrectScore === 2) {
                         document.getElementById("game-div").style.display = "none";
                         document.getElementById("postgame-div").style.display = "flex";
                         document.getElementById("lose-text").style.display = "flex";
                     }
-                    // TODO: add pause here to give player time to prepare for next question
-                    setCurrQuestionIndex(currQuestionIndex + 1);
-                    document.getElementById("light").style.backgroundColor = GREEN;
+                    // No answer but game not over; pause on red light before next question
+                    setTimeout(() => {
+                        document.getElementById("light").style.backgroundColor = GREEN;
+                        setCurrQuestionIndex((currQuestionIndex + 1) % questions.length);
+                        document.getElementById("question-div").style.display = "flex";
+                    }, RED_LIGHT_PAUSE_TIME);
                 }
             }
         }, questionResponseTime);
         return () => clearInterval(interval);
-    }, [currQuestionIndex, currCorrectScore, currIncorrectScore, questionResponseTime]); // if one of these changes, then the interval will be reset
+    }, [currQuestionIndex, currCorrectScore, currIncorrectScore, questionResponseTime, questions.length]); // if one of these changes, then the interval will be reset
 
     function submitAnswer() {
         const submission = document.getElementById('answer-input').value;
@@ -86,16 +91,25 @@ function RedLightGreenLightPage() {
                 document.getElementById("postgame-div").style.display = "flex";
                 document.getElementById("win-text").style.display = "flex";
             }
+            setCurrQuestionIndex((currQuestionIndex + 1) % questions.length);
         } else {
             document.getElementById('score-view').childNodes[currIncorrectScore].classList.add('red');
+            document.getElementById("light").style.backgroundColor = RED;
+            document.getElementById("question-div").style.display = "none";
             setCurrIncorrectScore(currIncorrectScore + 1);
             if (currIncorrectScore === 2) {
                 document.getElementById("game-div").style.display = "none";
                 document.getElementById("postgame-div").style.display = "flex";
                 document.getElementById("lose-text").style.display = "flex";
             }
+            // Incorrect answer but game not over; pause on red light before next question
+            setTimeout(() => {
+                document.getElementById("light").style.backgroundColor = GREEN;
+                setCurrQuestionIndex((currQuestionIndex + 1) % questions.length);
+                document.getElementById("question-div").style.display = "flex";
+            }, RED_LIGHT_PAUSE_TIME);
         }
-        setCurrQuestionIndex((currQuestionIndex + 1) % questions.length);
+
     }
 
     console.log(questions[currQuestionIndex]);
@@ -104,7 +118,7 @@ function RedLightGreenLightPage() {
         <div className="rlgl-full-container">
             <div className="empty-div"></div>
             <div className="red-light-green-light-container">
-                {console.log("rerender")}
+                {/* {console.log("rerender")} */}
                 <img className="background-image" src={RedLightGreenLightImage} alt="Red Light Green Light" />
                 <h1>{unit.name}</h1>
                 <div id="pregame-div">
@@ -117,11 +131,13 @@ function RedLightGreenLightPage() {
                     <div id="score-view">
                         <ScoreView />
                     </div>
+                    <div id="question-div">
                     <h4>Question:</h4>
-                    <h2>What is <span id="question-text">{questions[currQuestionIndex].english}</span> in Korean?</h2>
-                    <div id='answer'>
-                        <input id='answer-input' type='text' autoComplete='off' />
-                        <button className='btn btn-primary' onClick={submitAnswer}>Submit</button>
+                        <h2>What is <span id="question-text">{questions[currQuestionIndex].english}</span> in Korean?</h2>
+                        <div id='answer'>
+                            <input id='answer-input' type='text' autoComplete='off' />
+                            <button className='btn btn-primary' onClick={submitAnswer}>Submit</button>
+                        </div>
                     </div>
                 </div>
                 <div id='postgame-div'>
