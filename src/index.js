ff6b5c5bf61091e20975e517f423185e9012fff5
Rina Yoo <rinyou321@gmail.com>
2022-11-16 19:21:45 -0500

    updated about pg 1/2

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/Home';
+import AboutPage from './components/AboutPage/About';
 import Navbar from './components/Navbar/Navbar';
 import RedLightGreenLightPage from './components/RedLightGreenLightPage/RedLightGreenLightPage';
 import StudyOptionsPage from './components/StudyOptionsPage/StudyOptionsPage';
 import TugOfWarPage from './components/TugOfWarPage/TugOfWarPage';
 import UnitsListPage from './components/UnitsListPage/UnitsListPage';
 import FlashcardStudyPage from './components/FlashcardPage/FlashcardStudyPage';
-import { HOME_PATH, RED_LIGHT_GREEN_LIGHT_PATH, STUDY_OPTIONS_PATH, TUG_OF_WAR_PATH, UNITS_LIST_PATH, FLASHCARD_STUDY_PATH } from './constants';
+import { HOME_PATH, ABOUT_PATH, RED_LIGHT_GREEN_LIGHT_PATH, STUDY_OPTIONS_PATH, TUG_OF_WAR_PATH, UNITS_LIST_PATH, FLASHCARD_STUDY_PATH } from './constants';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
@@ -17,6 +18,7 @@ root.render(
         <Navbar></Navbar>
         <Routes>
             <Route path={HOME_PATH} element={<HomePage />} />
+            <Route path={ABOUT_PATH} element={<AboutPage />} />
             <Route path={UNITS_LIST_PATH} element={<UnitsListPage />} />
             <Route path={STUDY_OPTIONS_PATH} element={<StudyOptionsPage />} />
             <Route path={RED_LIGHT_GREEN_LIGHT_PATH} element={<RedLightGreenLightPage />} />
