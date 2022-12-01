e0160adc390b82e911280559b23eaf92a2d196f7
Ishawn Gullapalli <50810904+ishawng@users.noreply.github.com>
2022-12-01 18:42:10 -0500

    Add audio to flash cards

--- src/components/FlashcardPage/Flashcard.jsx
+++ src/components/FlashcardPage/Flashcard.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { getAudio } from '../../audioData';
+import { ReactComponent as SoundSvg } from './sound.svg';
 
 const Flashcard = ({ flashcard }) => {
-    const [flip, setFlip] = useState(false)
+    const [flip, setFlip] = useState(false);
+    const sound = getAudio(flashcard.korean) === null ? null : new Audio(require(`../../audios/words/${getAudio(flashcard.korean)}`));
+    const playSound = (event) => {
+        event.stopPropagation();
+        sound.play();
+    };
 
     return (
-        <div className={`card ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
+        <div className={`card${flip ? ' flip' : ''}`} onClick={() => setFlip(!flip)}>
             <div className='front'>
-                {flashcard.english}
-
+                <div className='content-container'><div className='content'>{flashcard.english}</div></div>
+                {sound !== null &&
+                    <div className='sound' onClick={playSound}>
+                        <SoundSvg />
+                    </div>
+                }
             </div>
-            <div className='back'>{flashcard.korean}</div>
-        </div>
-    )
+            <div className='back'>
+                <div className='content-container'><div className='content'>{flashcard.korean}</div></div>
+                {sound !== null &&
+                    <div className='sound' onClick={playSound}>
+                        <SoundSvg />
+                    </div>
+                }
+            </div>
+        </div >
+    );
 }
 
-export default Flashcard
+export default Flashcard;
